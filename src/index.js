085109cee5a0da2dceef2088a8581de6ca71c663
Juan Gomes Macário <jgomesmacario@outlook.com>
2023-05-14 03:27:28 -0300

    show ip address

--- src/index.js
+++ src/index.js
@@ -5,8 +5,26 @@ const http = require('http');
 const socketIo = require('socket.io');
 const robot = require('robotjs');
 const fs = require('fs');
+const os = require('os');
 
 let server;
+
+const filename = 'config.json';
+let config;
+
+// Verifica se o arquivo existe
+if (fs.existsSync(filename)) {
+  // Se existir, lê o conteúdo do arquivo
+  const data = fs.readFileSync(filename);
+  config = JSON.parse(data);
+} else {
+  // Se não existir, cria um arquivo vazio
+  let content = {
+    gamePath: "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Counter-Strike Global Offensive"
+  }
+  fs.writeFileSync(filename, JSON.stringify(content));
+  config = content;
+}
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -20,6 +38,8 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
     },
   });
 
@@ -30,8 +50,27 @@ const createWindow = () => {
   mainWindow.webContents.openDevTools();
 };
 
+getLocalIpAddress = () => {
+  const interfaces = os.networkInterfaces();
+
+  for (const iface of Object.values(interfaces)) {
+    for (const address of iface) {
+      if (address.family === 'IPv4' && !address.internal && address.address.startsWith('192.168.5.')) {
+        return address.address;
+      }
+    }
+  }
+
+  return null;
+}
+
 createServer = () => {
   const eServer = express();
+
+  eServer.get('/api/server-address', (req, res) => {
+    res.json({message: getLocalIpAddress()});
+  });
+
   server = http.createServer(eServer);
   const io = socketIo(server, {
     cors: {
@@ -45,7 +84,7 @@ createServer = () => {
 
     socket.on('say', function (say) {
       console.log('Received bind:', say);
-      fs.writeFile('C:\\Program Files (x86)\\Steam\\steamapps\\common\\Counter-Strike Global Offensive\\csgo\\cfg\\csconfig.cfg', `bind . "say ${say}"`, (err) => {
+      fs.writeFile(`${config.gamePath}\\csgo\\cfg\\csconfig.cfg`, `bind . "say ${say}"`, (err) => {
         if (err) throw err;
         console.log(`bind . "say ${say}"`);
         robot.keyTap(',');
@@ -58,8 +97,8 @@ createServer = () => {
     });
   });
 
-  server.listen(443, '192.168.5.69', function () {
-    console.log('Server listening on port 3000');
+  server.listen(443, getLocalIpAddress(), function () {
+    console.log('Server listening on port 443');
   });
 }
 
@@ -91,3 +130,6 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+// Evento que é disparado quando a aplicação está pronta
+// para receber mensagens do processo de renderização.
